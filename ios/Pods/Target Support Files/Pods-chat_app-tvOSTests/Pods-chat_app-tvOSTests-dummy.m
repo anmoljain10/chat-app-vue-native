9bf8b49e0aa19c3589b7bb2d113c80e66336dfd9
Anmol Jain <anmol@geekyants.com>
2019-12-31 13:04:07 +0530

    Pods removed

--- ios/Pods/Target Support Files/Pods-chat_app-tvOSTests/Pods-chat_app-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_chat_app_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_chat_app_tvOSTests
-@end
--- ios/Pods/Target Support Files/Pods-chat_appTests/Pods-chat_appTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_chat_appTests : NSObject
-@end
-@implementation PodsDummy_Pods_chat_appTests
-@end
